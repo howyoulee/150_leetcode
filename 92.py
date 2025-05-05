95884d04ccc1304d763e52fa14f1dd491cd04cf9
howyoulee <apandaseeds@foxmail.com>
2025-05-04 21:55:16 -0700

    5.4

--- 92.py
+++ 92.py
@@ -3,6 +3,7 @@ from typing import Optional
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
+        self.next = next
 
 def reverseBetween(self, head, left, right):
     """
